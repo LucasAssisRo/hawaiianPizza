bd81368554bac6783c79d83e734f0846e6e6db7e
LucasAssisRo <lucas.assis.ro@gmail.com>
2018-04-23 16:43:58 +0200

    first commit

---
+++ Margheritttta/Margheritttta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Margheritttta
+//
+//  Created by Lucas Assis Rodrigues on 23/04/2018.
+//  Copyright © 2018 Lucas Assis Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
