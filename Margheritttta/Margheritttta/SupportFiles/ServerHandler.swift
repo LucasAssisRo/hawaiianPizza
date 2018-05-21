b615c554b642b83b309720f18d79891e6a229eea
LucasAssisRo <lucas.assis.ro@gmail.com>
2018-05-21 14:46:00 +0200

    starting kitura kit

---
+++ Margheritttta/Margheritttta/SupportFiles/ServerHandler.swift
@@ -0,0 +1,19 @@
+//
+//  SeverHandler.swift
+//  Margheritttta
+//
+//  Created by Lucas Assis Rodrigues on 21/05/2018.
+//  Copyright © 2018 Lucas Assis Rodrigues. All rights reserved.
+//
+
+import UIKit
+import KituraKit
+
+final class ServerHandler {
+    public static let shared = KituraConnection()
+
+    private let url = "https://capricciosa.herokuapp.com"
+    private init() {}
+    
+    
+}
