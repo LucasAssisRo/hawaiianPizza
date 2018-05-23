fb85149e16446feac28649df179b7e3b658532f5
Alexander Schülke <schuelke.alexander@gmail.com>
2018-05-23 18:10:04 +0200

    Add files for cover collection view

---
+++ Margheritttta/Margheritttta/View/CollectionViewCells/Source/CoverCollectionViewItem.swift
@@ -0,0 +1,16 @@
+//
+//  CoverCollectionViewItem.swift
+//  Margheritttta
+//
+//  Created by Alexander Schülke on 21.05.18.
+//  Copyright © 2018 Lucas Assis Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class CoverCollectionViewItem: UICollectionViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+
